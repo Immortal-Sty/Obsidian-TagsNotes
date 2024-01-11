8b76c0af4573e788282772f9fffbf0a55cfc10ff
Immortal <immortalsty@qq.com>
2024-01-11 18:11:47 +0800

    auto: 2024.01.11 18:11:47 M .obsidian/appearance.json .obsidian/hotkeys.json .obsidian/plugins/quickadd/data.json Template/JavaScript/Toggle Snippets.js, A Template/JavaScript/Demo.js

--- Template/JavaScript/Demo.js
+++ Template/JavaScript/Demo.js
@@ -1,7 +1,7 @@
 const TEXT_FIELD = "Text field";
 
 module.exports = {
-	entry: async (QuickAdd, settings, params) => {
+	entry: async (QuickAdd, settings) => {
 		// Logic here
 		const textFieldSettingValue = settings[TEXT_FIELD];
 	},
@@ -35,23 +35,4 @@ module.exports = {
 			},
 		}
 	},
-	const snippetName = "状态栏自动隐藏_外部";
-	const snippetPath = app.customCss.getSnippetPath(snippetName);
-	if (!snippetPath) {
-		new Notice(`Snippet ${snippetName} not found`);
-	}
-
-	const isSnippetsEnabled = app.customCss.enabledSnippets.has(snippetName)
-	? true
-	: false;
-
-	if (isSnippetsEnabled) {
-		console.log("关闭");
-		app.customCss.setCssEnabledStatus(snippetName, false);
-		app.customCss.requestLoadSnippets();
-	} else {
-		console.log("启动");
-		app.customCss.setCssEnabledStatus(snippetName, true);
-		app.customCss.requestLoadSnippets();
-	}
-}
+};
